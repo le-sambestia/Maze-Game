c2aaa0289b9cc2dde8116607a808ee9887ec4566
Samuel Best <515985@escg.ac.uk>
2020-10-22 16:16:06 +0100

    no message

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -10,9 +10,21 @@ public class PlayerMovement : MonoBehaviour
     public float gravity = -9.81f;
 
     Vector3 velocity;
+
+    public Transform groundCheck;
+    public float groundDistance = 0.4f;
+    public LayerMask groundMask;
+
+    bool isGrounded;
     // Update is called once per frame
     void Update()
     {
+        isGrounded = Physics.CheckSphere(groundCheck.position, groundDistance, groundCheck Mask);
+
+        if (isGrounded && velocity.y < 0)
+        {
+            velocity.y = -2f;
+        }
         float x = Input.GetAxis("Horizontal");
         float z = Input.GetAxis("Vertical");
 
