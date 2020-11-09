a1e1eb76e899c0474d7fc8c4a21f4718f782d4cc
le_sambestia <515985@chrome.escg.ac.uk>
2020-11-09 20:02:53 +0000

    nearly finished maze gen script. still have problems without square mazes

--- Assets/Scripts/Maze.cs
+++ Assets/Scripts/Maze.cs
@@ -1,22 +1,42 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Rendering;
+using UnityEngine.UI;
 
 public class Maze : MonoBehaviour
 {
-    public int Rows = 4;
-    public int Columns = 4;
+    public int Rows = 2;
+    public int Columns = 2;
     public GameObject Wall;
     public GameObject Floor;
+    public InputField HeightField;
+    public InputField WidthField;
 
     private MazeCell[,] grid;
     private int currentRow = 0;
     private int currentColumn = 0;
+    private bool scanComplete = false;
 
-    // Start is called before the first frame update
     void Start()
     {
+        GenerateGrid();
+    }
+
+    void GenerateGrid()
+    {
+        //destroy all children of transform
+        foreach(Transform transform in transform)
+        {
+            Destroy(transform.gameObject);
+        }
+        //Dimension 2*2
         CreateGrid();
+
+        currentRow = 0;
+        currentColumn = 0;
+        scanComplete = false;
+
         HuntAndKill();
     }
 
@@ -44,7 +64,7 @@ public class Maze : MonoBehaviour
                 GameObject rightWall = Instantiate(Wall, new Vector3(j * size + 1.25f, 1.75f, -i * size), Quaternion.Euler(0, 90, 0));
                 rightWall.name = "RighttWall_" + i + "_" + j;
 
-                grid[i, j] = new MazeCell();
+                grid[i, j] = gameObject.AddComponent<MazeCell>();
                 grid[i, j].UpWall = upWall;
                 grid[i, j].DownWall = downWall;
                 grid[i, j].LeftWall = leftWall;
@@ -56,51 +76,41 @@ public class Maze : MonoBehaviour
                 leftWall.transform.parent = transform;
                 rightWall.transform.parent = transform;
 
+                if(i == 0 && j == 0)
+                {
+                    Destroy(leftWall);
+                }
+
+                if (i == Rows - 1 && j ==  Columns - 1)
+                {
+                    Destroy(rightWall);
+                }
             }
         }
     }
 
-    bool UnvisitedNeighbours()
-    {
-        //check up
-        if (IsUnvisited(currentRow - 1, currentColumn))
-        {
-            return true;
-        }
-
-        //check down
-        if (IsUnvisited(currentRow + 1, currentColumn))
-        {
-            return true;
-        }
-
-        //check left
-        if (IsUnvisited(currentRow, currentColumn + 1))
-        {
-            return true;
-        }
-
-        //check right
-        if (IsUnvisited(currentRow, currentColumn - 1))
-        {
-            return true;
-        }
-        return false;
-    }
-
-    bool IsUnvisited(int row, int column)
-    {
-        if (row >= 0 && row < Rows && column >= 0 && column < Columns && grid[row, column].Visited)
-        {
-            return true;
-        }
-        return false;
-    }
     void HuntAndKill()
     {
         grid[currentRow, currentColumn].Visited = true;
+        while(!scanComplete)
+        {
+            Walk();
+            Hunt();
+        }
+    }
 
-        while (UnvisitedNeighbours())
+    bool IsCellUnvisitedAndWithinBoundries(int row, int column)
+    {
+        if (row >= 0 && row < Rows && column >= 0 && column < Columns && !grid[row, column].Visited)
+        {
+            return true;
+        }
+        return false;
+    }
+
+    void Walk()
+    {
+        while (AreThereUnvisitedNeighbors())
         {
             int direction = Random.Range(0, 4);
 
@@ -108,7 +118,7 @@ public class Maze : MonoBehaviour
             if (direction == 0)
             {
                 Debug.Log("Check Up");
-                if (IsUnvisited(currentRow - 1, currentColumn))
+                if (IsCellUnvisitedAndWithinBoundries(currentRow - 1, currentColumn))
                 {
                     if (grid[currentRow, currentColumn].UpWall)
                     {
@@ -129,7 +139,7 @@ public class Maze : MonoBehaviour
             else if (direction == 1)
             {
                 Debug.Log("Check Down");
-                if (IsUnvisited(currentRow + 1, currentColumn))
+                if (IsCellUnvisitedAndWithinBoundries(currentRow + 1, currentColumn))
                 {
                     if (grid[currentRow, currentColumn].DownWall)
                     {
@@ -150,7 +160,7 @@ public class Maze : MonoBehaviour
             else if (direction == 2)
             {
                 Debug.Log("Check Left");
-                if (IsUnvisited(currentRow, currentColumn - 1))
+                if (IsCellUnvisitedAndWithinBoundries(currentRow, currentColumn - 1))
                 {
                     if (grid[currentRow, currentColumn].LeftWall)
                     {
@@ -171,7 +181,7 @@ public class Maze : MonoBehaviour
             else if (direction == 3)
             {
                 Debug.Log("Check Right");
-                if (IsUnvisited(currentRow, currentColumn + 1))
+                if (IsCellUnvisitedAndWithinBoundries(currentRow, currentColumn + 1))
                 {
                     if (grid[currentRow, currentColumn].RightWall)
                     {
@@ -188,5 +198,178 @@ public class Maze : MonoBehaviour
                 }
             }
         }
+    }
+
+    void Hunt()
+    {
+        scanComplete = true;
+        for (int i = 0; i < Rows; i++)
+        {
+            for (int j = 0; j < Columns; j++)
+            {
+                if (!grid[i, j].Visited && AreThereVisitedNeighbors(i, j))
+                {
+                    scanComplete = false;
+                    currentRow = i;
+                    currentColumn = j;
+                    grid[currentRow, currentColumn].Visited = true;
+                    DestroyAdjacentWall();
+                    return;
+                }
+            }
+        }
+    }
+    void DestroyAdjacentWall()
+    {
+        bool destroyed = false;
+
+        while (!destroyed)
+        {
+            int direction = Random.Range(0, 4);
+            //check up
+            if (direction == 0)
+            {
+                if (currentRow > 0 && grid[currentRow - 1, currentColumn].Visited)
+                {
+
+                    if (grid[currentRow, currentColumn].UpWall)
+                    {
+                        Destroy(grid[currentRow, currentColumn].UpWall);
+                    }
+
+                    if (grid[currentRow - 1, currentColumn].DownWall)
+                    {
+                        Destroy(grid[currentRow - 1, currentColumn].DownWall);
+                    }
+
+                    destroyed = true;
+                }
+            }
+            //check down
+            else if (direction == 1)
+            {
+                if (currentRow > Rows - 1 && grid[currentRow + 1, currentColumn].Visited)
+                {
+                    if (grid[currentRow, currentColumn].DownWall)
+                    {
+                        Destroy(grid[currentRow, currentColumn].DownWall);
+                    }
+                    if (grid[currentRow + 1, currentColumn].UpWall)
+                    {
+                        Destroy(grid[currentRow + 1, currentColumn].UpWall);
+                    }
+                    destroyed = true;
+                }
+            }
+            //check left
+            else if (direction == 2)
+            {
+                if (currentColumn > 0 && grid[currentRow, currentColumn - 1].Visited)
+                {
+
+                    if (grid[currentRow, currentColumn].LeftWall)
+                    {
+                        Destroy(grid[currentRow, currentColumn].LeftWall);
+                    }
+
+                    if (grid[currentRow, currentColumn - 1].RightWall)
+                    {
+                        Destroy(grid[currentRow, currentColumn - 1].RightWall);
+                    }
+                    destroyed = true;
+                }
+            }
+            //check right
+            else if (direction == 3)
+            {
+                if (currentColumn < Columns - 1 && grid[currentRow, currentColumn + 1].Visited)
+                {
+                    if (grid[currentRow, currentColumn].RightWall)
+                    {
+                        Destroy(grid[currentRow, currentColumn].RightWall);
+                    }
+
+                    if (grid[currentRow, currentColumn + 1].LeftWall)
+                    {
+                        Destroy(grid[currentRow, currentColumn + 1].LeftWall);
+                    }
+                    destroyed = true;
+                }
+            }
+        }
+    }
+    bool AreThereUnvisitedNeighbors()
+    {
+        //check up
+        if (IsCellUnvisitedAndWithinBoundries(currentRow - 1, currentColumn))
+        {
+            return true;
+        }
+
+        //check down
+        if (IsCellUnvisitedAndWithinBoundries(currentRow + 1, currentColumn))
+        {
+            return true;
+        }
+
+        //check left
+        if (IsCellUnvisitedAndWithinBoundries(currentRow, currentColumn + 1))
+        {
+            return true;
+        }
+
+        //check right
+        if (IsCellUnvisitedAndWithinBoundries(currentRow, currentColumn - 1))
+        {
+            return true;
+        }
+        return false;
+    }
+
+    public bool AreThereVisitedNeighbors(int row, int column)
+    {
+        //check up
+        if (row > 0 && grid[row - 1, column].Visited)
+        {
+            return true;
+        }
+
+        //check down
+        if (row < Rows - 1 && grid[row + 1, column].Visited)
+        {
+            return true;
+        }
+
+        //check left
+        if (column > 0 && grid[row, column - 1].Visited)
+        {
+            return true;
+        }
+
+        //check right
+        if (column < Columns - 1 && grid[row, column - 1].Visited)
+        {
+            return true;
+        }
+
+        return false;
+    }
+
+    public void Regenerate()
+    {
+        int rows = 2;
+        int columns = 2;
+
+        if (int.TryParse(HeightField.text, out rows))
+        {
+            Rows = rows;
+        }
+
+        if (int.TryParse(WidthField.text, out columns))
+        {
+            Columns = columns;
+        }
+
+        GenerateGrid();
     }
 }
